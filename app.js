fab5a38fab316ce02942c60d6c3026337aad2546
= <wlehoapt@gmail.com>
2021-01-15 00:58:08 +0900

    Init hapi module and add simple route function which broadcast post request body

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const config = require('./config');
+let channel;
+
+client.on('ready', () => {
+  // temporary initialize
+  channel = client.channels.cache.find(channel => channel.name === '일반');
+  // channel.send("Hello there!"); 
+});
 
 // all server message parsing.
 client.on('message', async msg => {
@@ -11,3 +21,32 @@ client.on('message', async msg => {
 });
 
 client.login(config.discord.id);
+
+const init = async () => {
+  
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+  });
+  
+  server.route({
+    method: 'POST',
+    path: '/msg',
+    handler: (request, h) => {
+      console.log(`${request.payload.msg}`);
+      channel.send(`request received: ${request.payload.msg}`);
+      return `${request.payload.msg}`;
+    }
+  });
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+  console.log(err);
+  process.exit(1);
+});
+
+init();
