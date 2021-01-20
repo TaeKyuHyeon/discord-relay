8cbb4cdd15826b036a1d651c9afc54a277a79c92
= <wlehoapt@gmail.com>
2021-01-20 22:56:47 +0900

    Change server ip, localhost to 0.0.0.0

--- server/index.js
+++ server/index.js
@@ -3,4 +3,4 @@
 console.log('Server init start');
 const Server = require("./server");
 // todo : config
-module.exports = new Server('localhost', 3000);
+module.exports = new Server('0.0.0.0', 3000);
