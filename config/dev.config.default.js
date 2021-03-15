f2010380a935f3f85248988adc00c68f59a80d8f
wlehoapt <wlehoapt@gmail.com>
2021-03-15 22:15:18 +0900

    Default config scheme update

--- config/dev.config.default.js
+++ config/dev.config.default.js
@@ -1,7 +1,10 @@
 'use strict';
 
 module.exports = {
+    server : {
+        logger : 'debug'
+    },
     discord : {
-        id: 'token'
+        id: 'ODE5MTI5ODgwMjIyNDk4ODQ3.YEiIJg.fIRndMpFm-9q8l3IM17cPI9aS5U'
     }
 }
