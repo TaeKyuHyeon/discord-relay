f4ca5cbd34b55c79ae61e74ba6ac3d0ba2da1808
wlehoapt <wlehoapt@gmail.com>
2021-03-17 18:58:25 +0900

    Add response module for #7 Improve response

--- modules/discord/discord-controller.js
+++ modules/discord/discord-controller.js
@@ -2,10 +2,12 @@
 
 const Logger = require('../../common/logger');
 const logger = new Logger(module);
+const Response = require('../../common/util/response');
 
 class DiscordController {
 
   constructor(bot) {
+    this.id = 101; // test
     this.bot = bot;
 
     this.routes  = () => {
@@ -13,38 +15,52 @@ class DiscordController {
         method: 'POST',
         path: '/discord',
         handler: async (request, h) => {
-          const channelName = request.payload.channel;
-          const msg = request.payload.msg;
+          const res = new Response(this.id, h);
 
-          var channel = this.bot.findChannel(channelName);
+          const channelName = request.payload.channel;
+          const requestMsg = request.payload.msg;
+
+          //var channel = this.bot.findChannel(channelName);
+          var channel = undefined;
           if (channel === undefined)
           {
-            logger.error(`invalid channel : ${channelName}`);
-            return 'fail';
+            const errMsg = `invalid channel : ${channelName}`;
+            logger.error(errMsg);
+            return res.fail(500, errMsg); 
           }
           
-          await channel.send(`request received: ${msg}`);
-          return msg;
+          await channel.send(`request received: ${requestMsg}`);
+          
+          const msg = `Send message to (${channel.id})${channel.channelName} success.`;
+          logger.debug(requestMsg);
+          let r = res.success(requestMsg);
+          return r;
         },
       },
       {
         method: 'POST',
         path: '/discord/messages',
         handler: async (request, h) => {
+          const res = new Response(this.id, h);
+
           const channelIds = request.payload.channelIds;
-          const msg = request.payload.msg;
+          const requestMsg = request.payload.msg;
 
           var channels = this.bot.findTextChannelByIds(channelIds);
           if (channels === undefined || channels.length === 0)
           {
-            logger.error(`failed to find channel id: ${channelIds.join()}`);
-            return 'fail';
+            const errMsg = `failed to find channel id: ${channelIds.join()}`;
+            logger.error(errMsg);
+            return res.fail(500, errMsg);
           }
           
           channels.forEach(async element => {
-            await element.send(`request received: ${msg}`);
+            await element.send(`request received: ${requestMsg}`);
           });
-          return msg;
+
+          const msg = `Send message to channels(${channelIds.join()}) success.`;
+          logger.debug(msg);
+          return res.success(msg);
         },
       }];
     };
